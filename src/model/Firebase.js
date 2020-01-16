6c06d023d492a5d4ba9b30ae08824bb04a95efeb
albertrein <guilhermealberto8@gmail.com>
2020-01-16 07:46:53 -0300

    Fixing file exemple.

--- src/model/Firebase.js
+++ src/model/Firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-var serviceAccount = require("../config/perfecta-83c3c-firebase-adminsdk-ll0wl-957a3d1645.json");
+var serviceAccount = require("../config/perfecta-83c3c-firebase-adminsdk-ll0wl-957a3d1645.json.exemple");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
